9feb646d38590179eb3308ce71482990dfba423b
Pete Cracknell <p3tecracknell@gmail.com>
2017-09-26 11:58:24 +0100

    Support to disable checking cert by env

--- server.js
+++ server.js
@@ -9,9 +9,11 @@ const google = require('./google')
 const port = process.env.PORT || 3000
 const expressApp = express()
 
+const checkCert = process.env.ALEXA_CHECK_CERT || true
+
 const alexa = require('./alexa')
 const alexa2 = require('./alexa2')
-alexa.express({ expressApp })
+alexa.express({ expressApp, checkCert })
 alexa2.express({ expressApp })
 
 expressApp.use(bodyParser.json());
